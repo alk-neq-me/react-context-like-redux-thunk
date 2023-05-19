579a0c71c3fe1130cd6f156e764287ddcc1fe427
marco <toyko2001@gmail.com>
2023-05-19 23:22:46 +0630

    test with async wait 1s create todo action

--- src/context/todo/todoAction.ts
+++ src/context/todo/todoAction.ts
@@ -1,10 +1,20 @@
 import { ActionAsync } from "../types";
 import { Todo, TodoActionEnum } from "./types";
 
+
 export function createTodo(todo: Todo): ActionAsync<TodoActionEnum, Todo|string> {
   return async (dispatch) => {
-    // dispatch({ type: "CREATE_TODOS_PENDING" });
-    dispatch({ type: "CREATE_TODOS_SUCCESS", payload: todo });
+    dispatch({ type: "CREATE_TODOS_PENDING" });
+
+    /* test with waiting promise */
+    await (new Promise((resolve, _reject) => {
+      setTimeout(() => {
+        resolve(
+          dispatch({ type: "CREATE_TODOS_SUCCESS", payload: todo })
+        )
+      }, 1000);
+    }));
+
     // dispatch({ type: "CREATE_TODOS_FAILURE", payload: "unknow error" });
   };
 }
